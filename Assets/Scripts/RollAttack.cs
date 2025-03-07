8537984f349999308b1647c6bfa0a95e91106a76
Colby Thompson <colbykade626@gmail.com>
2025-03-07 15:57:49 -0600

    Groundwork for Attack Button

---
+++ Assets/Scripts/RollAttack.cs
@@ -0,0 +1,31 @@
+using System.Text;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RollAttack : MonoBehaviour
+{
+    public ToggleGroup weaponTypeGroup;
+    public ToggleGroup weaponSizeGroup;
+    public ToggleGroup weaponMaterialGroup;
+    public ToggleGroup otherMaterialGroup;
+    public ToggleGroup enchantmentLevelGroup;
+    public TextMeshProUGUI combatLog;
+
+    // Called by button 
+    public void AttackRoll()
+    {
+		StringBuilder stringBuilder = new StringBuilder();
+        var weaponType = weaponTypeGroup.GetFirstActiveToggle();
+		stringBuilder.Append("Attack type: " + weaponType.name + "\n");
+		var weaponSize = weaponSizeGroup.GetFirstActiveToggle();
+		stringBuilder.Append("Weapon size: " + weaponSize.name + "\n");
+		var weaponMaterial = weaponMaterialGroup.GetFirstActiveToggle();
+		stringBuilder.Append("Weapon material: " + weaponMaterial.name + "\n");
+		var otherMaterial = otherMaterialGroup.GetFirstActiveToggle();
+		stringBuilder.Append("Other material: " + otherMaterial.name + "\n");
+		var enchantmentLevel = enchantmentLevelGroup.GetFirstActiveToggle();
+		stringBuilder.Append("Enchantment level: " + enchantmentLevel.name + "\n\n");
+		combatLog.text += stringBuilder.ToString();
+	}
+}
---
+++ Assets/Scripts/Utility/ToggleChange.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleChange : MonoBehaviour
+{
+    public Toggle meleeToggle;
+    public Toggle rangedToggle;
+    public Toggle magicToggle;
+
+    public ToggleGroup otherMaterialGroup;
+    public ToggleGroup enchantmentLevelGroup;
+
+    public void OnToggleValueChanged()
+    {
+        if (meleeToggle.isOn)
+        {
+            foreach (Transform child in otherMaterialGroup.transform)
+            {
+                child.GetComponent<Toggle>().interactable = false;
+            }
+			foreach (Transform child in enchantmentLevelGroup.transform)
+			{
+				child.GetComponent<Toggle>().interactable = true;
+			}
+		}
+		else if (rangedToggle.isOn)
+		{
+			foreach (Transform child in otherMaterialGroup.transform)
+			{
+				child.GetComponent<Toggle>().interactable = true;
+			}
+			foreach (Transform child in enchantmentLevelGroup.transform)
+			{
+				child.GetComponent<Toggle>().interactable = true;
+			}
+		}
+		else if (magicToggle.isOn)
+		{
+			foreach (Transform child in otherMaterialGroup.transform)
+			{
+				child.GetComponent<Toggle>().interactable = false;
+			}
+			foreach (Transform child in enchantmentLevelGroup.transform)
+			{
+				child.GetComponent<Toggle>().interactable = false;
+			}
+		}
+		else
+		{
+			Debug.LogError("Weapon type is undefined");
+		}
+	}
+}
